6a17f44780a83fa71d109af157980b606629531d
Alexander Karlsson <alexander.karlsson@nexergroup.com>
2023-03-03 15:43:25 +0100

    first

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.viewport(2400, 1280)
+    cy.visit('https://www.webhallen.com/')
+    cy.get('.cookie-buttons > :nth-child(1)').click()
+    cy.get('[alt="login-toggle"]').click()
+  })
+})
